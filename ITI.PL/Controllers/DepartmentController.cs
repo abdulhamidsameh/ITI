2e7b41f4f3cfeb0f9334c5574ee2757c623ffbfd
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-20 08:15:54 +0300

    Department Controller Details

--- ITI.PL/Controllers/DepartmentController.cs
+++ ITI.PL/Controllers/DepartmentController.cs
@@ -9,13 +9,13 @@ namespace ITI.PL.Controllers
 		private readonly IDepartmentRepository _departmentRepo;
 
 		public DepartmentController(IDepartmentRepository departmentRepo)
-        {
+		{
 			_departmentRepo = departmentRepo;
 		}
 
 		// baseUrl/Department/Index
 		[HttpGet]
-        public IActionResult Index()
+		public IActionResult Index()
 		{
 			var departments = _departmentRepo.GetAll();
 			return View(departments);
@@ -34,12 +34,28 @@ namespace ITI.PL.Controllers
 			if (ModelState.IsValid)
 			{
 				var Count = _departmentRepo.Add(model);
-				if(Count > 0)
+				if (Count > 0)
 					return RedirectToAction(nameof(Index));
-				
+
 			}
 
 			return View(model);
+
+		}
+
+		// baseUrl/Department/Details/id?
+		[HttpGet]
+		public IActionResult Details(int? id)
+		{
+			if (!id.HasValue)
+				return BadRequest();
+
+			var department = _departmentRepo.Get(id.Value);
+
+			if (department is null)
+				return NotFound();
+
+			return View(department);
 
 		}
 	}
