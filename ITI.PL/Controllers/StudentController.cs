25641a75a349498f0f5c6ea697c45b3f5dbec1ab
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-20 12:08:41 +0300

    Student Controller

---
+++ ITI.PL/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using ITI.BLL.Interfaces;
+using ITI.DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.PL.Controllers
+{
+	public class StudentController : Controller
+	{
+		private readonly IStudentRepository _repository;
+
+		public StudentController(IStudentRepository repository)
+        {
+			_repository = repository;
+		}
+
+		// BaseUrl/Student/Index
+		[HttpGet]
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
