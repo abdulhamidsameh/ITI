ee018f1b23e1f7d68b622f50bb8bb59368dd6ebb
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-21 23:33:56 +0300

    Close Buttons To Hide Div

--- ITI.PL/wwwroot/js/site.js
+++ ITI.PL/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function Change() {
+    var div = document.getElementById("myDiv");
+    div.style.display = "none";
+}
